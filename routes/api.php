c4e85469de623fdcb9d30dccc2756734e7d76897
Vedant <vedant@slick-digital.co.uk>
2023-10-04 09:43:35 +0100

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::group([
+    'prefix' => 'order'
+], function () {
+    Route::post('add', 'App\Http\Controllers\OrderController@store');
+    Route::get('list', 'App\Http\Controllers\OrderController@index');
+    Route::get('show/{id}', 'App\Http\Controllers\OrderController@show');
+    Route::put('update/{id}', 'App\Http\Controllers\OrderController@update');
+    Route::delete('delete/{id}', 'App\Http\Controllers\OrderController@destroy');
+});
